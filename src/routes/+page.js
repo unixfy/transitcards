f0ebbf8774b6264ffb39bb2b87ec3637d93442d3
Alex <alex@unixfy.net>
2025-05-13 16:34:22 +0000

    Use aggregation function to get number of cards

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,6 +3,7 @@ import { readItems } from '@directus/sdk';
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	return {
-		transit_cards: await directus.request(readItems('transit_cards', { limit: 500, sort: ['date_acquired', 'name'], fields: ['id', 'name', 'date_acquired', 'image', 'issuing_agency.name', 'issuing_agency.city', 'notes'] })),
+		transit_cards: await directus.request(readItems('transit_cards', { sort: ['date_acquired', 'name'], fields: ['id', 'name', 'date_acquired', 'image', 'issuing_agency.name', 'issuing_agency.city', 'notes'] })),
+		count_transit_cards: await directus.request(readItems('transit_cards', { limit: -1, aggregate: { count: ['id'] } }))
 	};
 }
